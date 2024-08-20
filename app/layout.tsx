b36c08b46d3d6c96e8e978ffad3c2424a893f96a
Leonardo Castro <142045008+LeoCasttr0@users.noreply.github.com>
2024-08-20 01:12:43 -0300

    chore: add shadcn theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    // adiconando modo dark no body do html todo
+    <html lang="en" className="dark">
       <body className={inter.className}>{children}</body>
     </html>
   );
